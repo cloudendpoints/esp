65da4206aecfd9bb31858bb1affe234c8c74e82e
rkdirect <roger.knapp@vroom.com>
2017-10-30 16:29:33 -0500

    Added always_print_primitive_fields experimental option (#227) (#295)
    * Added always_print_primitive_fields experimental option (#227)
    
    * Fix file formatting in previous commit (#227)

--- src/api_manager/context/global_context.h
+++ src/api_manager/context/global_context.h
@@ -70,6 +70,14 @@ class GlobalContext {
     return false;
   }
 
+  bool AlwaysPrintPrimitiveFields() {
+    if (server_config() && server_config()->has_experimental()) {
+      const auto &experimental = server_config()->experimental();
+      return experimental.always_print_primitive_fields();
+    }
+    return false;
+  }
+
   // report interval can be override by server_config.
   int64_t intermediate_report_interval() const {
     return intermediate_report_interval_;
