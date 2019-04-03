1996eccd6bf3dd866575b8a192dc9ee236078280
Kyu Hyun Chang <kyuc@google.com>
2019-04-03 14:48:35 -0700

    Override Compute Platform in serverless docker image

--- docker/serverless/env-start-esp.py
+++ docker/serverless/env-start-esp.py
@@ -59,9 +59,10 @@ def serve_error_msg(error_msg):
 
 def main():
     CMD = "/usr/sbin/start_esp"
+    PLATFORM = "Cloud Run"
 
     # The command being run must be the 0th arg.
-    ARGS = [CMD, "--enable_backend_routing"]
+    ARGS = [CMD, "--enable_backend_routing", "--compute_platform_override={}".format(PLATFORM)]
 
     # Uncaught KeyError; if no port, we can't serve a nice error handler. Crash instead.
     assert_env_var("PORT")
