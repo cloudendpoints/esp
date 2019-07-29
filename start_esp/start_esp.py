48de8ab391aca7b46036d5fd58cc744ba0f751cc
Stipe <slatkovi@gmail.com>
2019-07-30 01:13:28 +0200

    Multi-service support

--- start_esp/start_esp.py
+++ start_esp/start_esp.py
@@ -163,6 +163,8 @@ def write_nginx_conf(ingress, nginx_conf, args):
             server_config_path=args.server_config_path,
             experimental_proxy_backend_host_header=args.experimental_proxy_backend_host_header,
             enable_strict_transport_security=args.enable_strict_transport_security,
+            service_names=args.service,
+            config_dir=args.config_dir,
             google_cloud_platform=(args.non_gcp==False))
 
     # Save nginx conf
