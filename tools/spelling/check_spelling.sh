ec9ce399ae03158baa39337d8cfaffa166184da9
Xuyang Tao <taoxuy@google.com>
2019-07-30 18:26:21 +0000

    add skipped files from third_party and move spelling check tool to tools/spelling

--- tools/spelling/check_spelling.sh
+++ tools/spelling/check_spelling.sh
@@ -62,9 +62,10 @@ else
   exit 1
 fi
 
-SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
-ROOTDIR="${SCRIPTPATH}/.."
+ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 cd "$ROOTDIR"
+
+
 
 BIN_FILENAME="misspell_"${VERSION}"_"${OS}"_64bit.tar.gz"
 # Install tools we need
@@ -96,11 +97,11 @@ fi
 chmod +x "${TMP_DIR}/misspell"
 
 # Spell checking
-# All the skipping files are defined in tools/spelling_skip_files.txt
-SPELLING_SKIP_FILES="${ROOTDIR}/tools/spelling_skip_files.txt"
+# All the skipping files are defined in tools/spelling/spelling_skip_files.txt
+SPELLING_SKIP_FILES="${ROOTDIR}/tools/spelling/spelling_skip_files.txt"
 
-# All the ignore words are defined in tools/spelling_whitelist_words.txt
-SPELLING_WHITELIST_WORDS_FILE="${ROOTDIR}/tools/spelling_whitelist_words.txt"
+# All the ignore words are defined in tools/spelling/spelling_whitelist_words.txt
+SPELLING_WHITELIST_WORDS_FILE="${ROOTDIR}/tools/spelling/spelling_whitelist_words.txt"
 
 WHITELIST_WORDS=$(echo -n $(cat "${SPELLING_WHITELIST_WORDS_FILE}" | \
                   grep -v "^#"|grep -v "^$") | tr ' ' ',')
