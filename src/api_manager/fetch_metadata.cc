ba9c83c70e9bfc290f8f7402460044fb4f339a1d
Xuyang Tao <taoxuy@google.com>
2019-09-19 16:28:29 -0700

    metadata: add fetch failure status timeout
    The metadata fetch won't fetch after the first failure and
    set a failure time window, after which the proxy can fecth
    again.

--- src/api_manager/fetch_metadata.cc
+++ src/api_manager/fetch_metadata.cc
@@ -19,6 +19,7 @@
 #include "include/api_manager/http_request.h"
 #include "src/api_manager/auth/lib/auth_token.h"
 
+using std::chrono::system_clock;
 using ::google::api_manager::utils::Status;
 using ::google::protobuf::util::error::Code;
 
@@ -38,7 +39,8 @@ const char kMetadataInstanceIdentityToken[] =
 // The maximum lifetime of a cache token. Unit: seconds.
 // Token expired in 1 hour, reduce 100 seconds for grace buffer.
 const int kInstanceIdentityTokenExpiration = 3500;
-
+// Time window (in seconds) of failure status after a failed fetch.
+const int kFailureStatusWindow = 5;
 // Initial metadata fetch timeout (1s)
 const int kMetadataFetchTimeout = 1000;
 // Maximum number of retries to fetch token from metadata
@@ -116,9 +118,13 @@ void GlobalFetchServiceAccountToken(
       }
       break;
     case auth::ServiceAccountToken::FAILED:
-      // permanent failure
-      continuation(Status(Code::INTERNAL, kFailedTokenFetch));
-      return;
+      // If the current time doesn't get the time window of failure status,
+      // it will return kFailedTokenFetch directly.
+      if (system_clock::now() - token->last_failed_fetch_time() < std::chrono::seconds(kFailureStatusWindow)) {
+        continuation(Status(Code::INTERNAL, kFailedTokenFetch));
+        return;
+      }
+      break;
     case auth::ServiceAccountToken::NONE:
     default:
       env->LogDebug("Need to fetch service account token");
@@ -132,6 +138,7 @@ void GlobalFetchServiceAccountToken(
                   // fetch failed
                   if (!status.ok()) {
                     env->LogDebug("Failed to fetch service account token");
+                    token->set_last_failed_fetch_time(system_clock::now());
                     token->set_state(auth::ServiceAccountToken::FAILED);
                     continuation(Status(Code::INTERNAL, kFailedTokenFetch));
                     return;
