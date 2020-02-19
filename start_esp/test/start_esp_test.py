4c86ee209f875a2e123f58459c39035f468822b9
Wayne Zhang <qiwzhang@google.com>
2020-02-19 14:27:46 -0800

    Add flag --client_body_timeout (#765)

--- start_esp/test/start_esp_test.py
+++ start_esp/test/start_esp_test.py
@@ -153,6 +153,11 @@ class TestStartEsp(unittest.TestCase):
         config_generator = self.basic_config_generator + " --keepalive_timeout=600s"
         self.run_test_with_expectation(expected_config_file, self.generated_nginx_config_file, config_generator)
 
+    def test_client_body_timeout_output_is_as_expected(self):
+        expected_config_file = "./start_esp/test/testdata/expected_client_body_timeout_nginx.conf"
+        config_generator = self.basic_config_generator + " --client_body_timeout=86600s"
+        self.run_test_with_expectation(expected_config_file, self.generated_nginx_config_file, config_generator)
+
     def test_allow_invalid_headers_arg_output_is_as_expected(self):
         expected_config_file = "./start_esp/test/testdata/expected_allow_invalid_headers_nginx.conf"
         config_generator = self.basic_config_generator + " --allow_invalid_headers"
