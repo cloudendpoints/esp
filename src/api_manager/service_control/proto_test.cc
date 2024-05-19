1fbf9e7434e11f652e131f82f76cf0c06a90684a
Elliot-xq <kangda@google.com>
2024-05-19 17:35:59 +0000

    address comments

--- src/api_manager/service_control/proto_test.cc
+++ src/api_manager/service_control/proto_test.cc
@@ -374,7 +374,7 @@ TEST_F(ProtoTest, FillReportRequestEmptyOptionalTest) {
   ASSERT_EQ(expected_text, text);
 }
 
-TEST_F(ProtoTest, CredentailIdApiKeyTest) {
+TEST_F(ProtoTest, CredentialIdApiKeyTest) {
   ReportRequestInfo info;
   FillOperationInfo(&info);
 
@@ -385,7 +385,7 @@ TEST_F(ProtoTest, CredentailIdApiKeyTest) {
             "apikey:api_key_x");
 }
 
-TEST_F(ProtoTest, CredentailIdApiKeyUidTest) {
+TEST_F(ProtoTest, CredentialIdApiKeyUidTest) {
   ReportRequestInfo info;
   FillOperationInfo(&info);
   info.check_response_info.api_key_uid = "apikey:fake_uid";
@@ -397,7 +397,7 @@ TEST_F(ProtoTest, CredentailIdApiKeyUidTest) {
             "apikey:fake_uid");
 }
 
-TEST_F(ProtoTest, CredentailIdApiKeyUidUnknownTest) {
+TEST_F(ProtoTest, CredentialIdApiKeyUidUnknownTest) {
   ReportRequestInfo info;
   FillOperationInfo(&info);
   info.check_response_info.is_network_failure = true;
@@ -410,7 +410,7 @@ TEST_F(ProtoTest, CredentailIdApiKeyUidUnknownTest) {
             "apikey:UNKNOWN");
 }
 
-TEST_F(ProtoTest, CredentailIdIssuerOnlyTest) {
+TEST_F(ProtoTest, CredentialIdIssuerOnlyTest) {
   ReportRequestInfo info;
   FillOperationInfo(&info);
   info.api_key = "";
@@ -423,7 +423,7 @@ TEST_F(ProtoTest, CredentailIdIssuerOnlyTest) {
             "jwtauth:issuer=YXV0aC1pc3N1ZXI");
 }
 
-TEST_F(ProtoTest, CredentailIdIssuerAudienceTest) {
+TEST_F(ProtoTest, CredentialIdIssuerAudienceTest) {
   ReportRequestInfo info;
   FillOperationInfo(&info);
   info.api_key = "";
